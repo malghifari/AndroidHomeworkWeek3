5726fac45fe28859a44637690904605eda7bae69
alghi <mo.alghifari@gmail.com>
2019-02-14 23:10:37 +0700

    Parse JSON String

--- WhoWroteIt/app/src/main/java/com/example/alghifari/whowroteit/FetchBook.java
+++ WhoWroteIt/app/src/main/java/com/example/alghifari/whowroteit/FetchBook.java
@@ -3,6 +3,9 @@ package com.example.alghifari.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class FetchBook extends AsyncTask<String, Void, String> {
     private TextView mTitleText;
     private TextView mAuthorText;
@@ -19,5 +22,37 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            //Iterate through the results
+            for(int i = 0; i<itemsArray.length(); i++){
+                JSONObject book = itemsArray.getJSONObject(i); //Get the current item
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                //If both a title and author exist, update the TextViews and return
+                if (title != null && authors != null){
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+        } catch (Exception e){
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+
     }
 }
