442924eaf6233b4b4a104809113fe39bc401b65a
alghi <mo.alghifari@gmail.com>
2019-02-14 23:46:14 +0700

    Added a content intent

--- NotifyMe/app/src/main/java/com/example/alghifari/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/alghifari/notifyme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alghifari.notifyme;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,10 +29,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void sendNotification() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this)
                 .setContentTitle("You've been notified!")
                 .setContentText("This is your notification text.")
-                .setSmallIcon(R.drawable.ic_android);
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(notificationPendingIntent);
         Notification myNotification = notifyBuilder.build();
         mNotifyManager.notify(NOTIFICATION_ID, myNotification);
     }
