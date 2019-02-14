b0f7578e05471c72a9a96e69d370cdb93015f07b
alghi <mo.alghifari@gmail.com>
2019-02-14 23:40:28 +0700

    Added first notification

--- NotifyMe/app/src/main/java/com/example/alghifari/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/alghifari/notifyme/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.alghifari.notifyme;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.notify);
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+    }
+    public void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android);
+        Notification myNotification = notifyBuilder.build();
+        mNotifyManager.notify(NOTIFICATION_ID, myNotification);
     }
 }
